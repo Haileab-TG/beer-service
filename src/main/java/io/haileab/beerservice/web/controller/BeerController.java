66ff0b49db30b53de685bb769767d2e02a65e48d
Haileab-TG <haileab.t.g@gmail.com>
2023-10-18 23:23:40 -0500

    adding beer controller

---
+++ src/main/java/io/haileab/beerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package io.haileab.beerservice.web.controller;
+
+import io.haileab.beerservice.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId){
+        //todo impl
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO){
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateBeerById
+            (
+                @PathVariable("beerId") UUID beerId,
+                @RequestBody BeerDTO beerDTO
+            ){
+        //todo impl
+    }
+
+}
